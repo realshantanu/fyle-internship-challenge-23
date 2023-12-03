1c6cb3f6b74abdefa88c97f9f20a3cfbb008723a
Shantanu Rajmane <shantanurajmane_22992@aitpune.edu.in>
2023-12-03 09:08:34 +0530

    fixing hosting problem for page not found

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const TOKEN_KEY = {
   production: false,
-  githubApiKey: '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',
+  githubApiKey: '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',
 };
