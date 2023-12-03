efe0a8b11196a3821e84ed71451c4a0279d82569
Shantanu Rajmane <shantanurajmane_22992@aitpune.edu.in>
2023-12-03 09:41:01 +0530

    Update API service to use TOKEN_KEY from environment

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable, catchError, forkJoin, from, map, throwError } from 'rxjs';
 import { mergeMap } from 'rxjs/operators';
 import { Octokit } from '@octokit/rest';
+import { TOKEN_KEY } from '../environments/environment';
 
 /**
  * Service for making API requests.
@@ -21,7 +22,7 @@ export class ApiService {
   constructor(
     private httpClient: HttpClient
   ) {
-    this.octokit = new Octokit({auth: (process.env['TOKEN_KEY'])});
+    this.octokit = new Octokit({auth: (TOKEN_KEY.githubApiKey)});
   }
 
   /**
